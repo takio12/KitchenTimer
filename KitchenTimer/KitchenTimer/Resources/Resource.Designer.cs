0643265cf74fa65ec36b69aa6395400b189050ae
TAKIO-PC\takio <taka21centry@gmail.com>
2017-09-26 23:23:14 +0900

    タイマー、表示ボタン類追加

--- KitchenTimer/KitchenTimer/Resources/Resource.Designer.cs
+++ KitchenTimer/KitchenTimer/Resources/Resource.Designer.cs
@@ -57,6 +57,25 @@ namespace KitchenTimer
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int RemainingTimeTextView = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int StartButton = 2131034113;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
